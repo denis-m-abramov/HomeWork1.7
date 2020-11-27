d609e69b4b4312b254313e157f2c17d817043772
Denis Abramov <denis.m.abramov@mail.ru>
2020-11-27 14:14:41 +0300

    Change 1
    Change 1

--- HW7.playground/Contents.swift
+++ HW7.playground/Contents.swift
@@ -28,8 +28,16 @@ class Shape {
     var radius: Float = 0
     var square: Float = 0
     var perimeter: Float = 0
-    
     var nameShape: String = ""
+    
+    lazy var numberFormatter: NumberFormatter = {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = NumberFormatter.Style.decimal
+        numberFormatter.roundingMode = NumberFormatter.RoundingMode.up
+        numberFormatter.maximumFractionDigits = 2
+        numberFormatter.minimumFractionDigits = 2
+        return numberFormatter
+    }()
     
     init() {}
     
@@ -67,22 +75,28 @@ class Shape {
     }
     
     func description() {
-        print("Площадь фигуры \(nameShape) равна \(formatter.string(from: NSNumber(value: squareOfShap()))!), периметр (длина) равен \(formatter.string(from: NSNumber(value: perimeterOfShape()))!)")
+        print("Площадь фигуры \(nameShape) равна \(numberFormatter.string(from: NSNumber(value: squareOfShap()))!), периметр (длина) равен \(numberFormatter.string(from: NSNumber(value: perimeterOfShape()))!)")
     }
 }
 
+//Вариант без упаковки в свойство
+//let formatter = NumberFormatter()
+//formatter.numberStyle = NumberFormatter.Style.decimal
+//formatter.roundingMode = NumberFormatter.RoundingMode.up
+//formatter.maximumFractionDigits = 2
+//formatter.minimumFractionDigits = 2
+//print(formatter.string(from: NSNumber(value: orangeVolume.orangeVolume))!)
 
+//Форматирует дату
+//lazy var dateFormatter: DateFormatter = {
+//    let dateFormatter = DateFormatter()
+//    return dateFormatter
+//}()
 /*:
  1.2. Создайте классы `Circle`, `Rectangle` и `Ellipse`, унаследовав их от `Shape`. Переопределите методы `squareOfShape` и `perimeterOfShape` для каждого класса в соответствии с правилом расчета площади или периметра (длины) конкретной фигуры
  
  1.3 В каждом классе создайте метод `description`, который выводит на консоль сообщение следующего вида: «Площадь фигуры <тип фигуры> равна <... >, периметр (длина) равна <...>»
  */
-let formatter = NumberFormatter()
-formatter.numberStyle = NumberFormatter.Style.decimal
-formatter.roundingMode = NumberFormatter.RoundingMode.up
-formatter.maximumFractionDigits = 2
-formatter.minimumFractionDigits = 2
-
 
 class Circle: Shape {
     
@@ -197,4 +211,159 @@ ellipse2.checksTheValue()
  
  */
 
+/// Композиция
+struct AudioFile {
+    /// Название
+    let title: String
+    /// Альбом
+    let album: String
+    /// Автор
+    let author: String
+    /// Длина в секундах
+    let lengthSeconds: Int
+}
 
+/// Аудиоплеер
+class AudioPlayer {
+    /// Кол-во композиций
+    var numberOfCompositions: Int { compositions.count }
+    /// Массив композиций (плейлист)
+    private var compositions: [AudioFile] = []
+    /// Индекс текущей композиции
+    private var currentCompositionIndex: Int = 0
+    /// Текущее устройство для вывода звука, если nil, то устройство не подключено
+    private var currentOutputAudioDevice: OutputAudioDevice?
+
+    /// Подключить устройство для вывода звука
+    func setOutputDevice(_ device: OutputAudioDevice?) {
+        currentOutputAudioDevice = device
+    }
+
+    /// Добавить новые треки
+    func add(compositions: [AudioFile]) {
+        self.compositions.append(contentsOf: compositions)
+    }
+
+    /// Перемешать плейлист
+    func shuffleCompositions() {
+        compositions.shuffle()
+    }
+
+    // MARK: - Проигрывание
+
+    /// Включить текущуий трек
+    func playCurrentComposition() {
+        guard compositions.count > 0 else {
+            printNoCompositions()
+            return
+        }
+        play(composition: compositions[currentCompositionIndex])
+    }
+
+    /// Включить следующий трек
+    func playNext() {
+        guard compositions.count > 0 else {
+            printNoCompositions()
+            return
+        }
+        let nextIndex = currentCompositionIndex + 1 < compositions.count ? currentCompositionIndex + 1 : 0
+        currentCompositionIndex = nextIndex
+        let nextComposition = compositions[nextIndex]
+        play(composition: nextComposition)
+    }
+
+    /// Включить предыдущий трек
+    func playPrevious() {
+        guard compositions.count > 0 else {
+            printNoCompositions()
+            return
+        }
+        let previousIndex = currentCompositionIndex - 1 >= 0 ? currentCompositionIndex - 1 : compositions.count - 1
+        currentCompositionIndex = previousIndex
+        let previousComposition = compositions[previousIndex]
+        play(composition: previousComposition)
+    }
+
+    /// Проиграть композицию
+    private func play(composition: AudioFile) {
+        print("Играет песня \"\(composition.title)\" из альбома \"\(composition.album)\", исполнитель \"\(composition.author)\"")
+        currentOutputAudioDevice?.playSound(withLengthInSeconds: composition.lengthSeconds)
+    }
+
+    /// Вывести информацию о том, что композиций нет
+    private func printNoCompositions() {
+        print("Композиции не найдены")
+    }
+}
+
+// 2.2
+
+/// Протокол устройства для вывода звука
+protocol OutputAudioDevice {
+    /// Проиграть звук с длительностью
+    func playSound(withLengthInSeconds length: Int)
+}
+
+/// Выходное устройство - Наушники
+class Headphones: OutputAudioDevice {
+    func playSound(withLengthInSeconds length: Int) {
+        print("В наушниках проигрывается неизвестный звук длительностью \(length) с.")
+    }
+}
+
+/// Выходное устройство - Колонки
+class Loudspeakers: OutputAudioDevice {
+    func playSound(withLengthInSeconds length: Int) {
+        print("На колонках проигрывается неизвестный звук длительностью \(length) с.")
+    }
+}
+
+
+// 2.3
+
+// Создать объект класса проигрыватель(AudioPlayer)
+
+let myAudioPlayer = AudioPlayer()
+
+// Добавить в него 3 любые песни addCompositions, пример песни: AudioFile(title: "Anesthetize", album: "Fear of a Blank Planet", author: "Porcupine Tree", lengthSeconds: 1063)
+
+myAudioPlayer.add(compositions: [
+    AudioFile(title: "Anesthetize", album: "Fear of a Blank Planet", author: "Porcupine Tree", lengthSeconds: 1063),
+    AudioFile(title: "Афоризмы житейской мудрости", album: "Аудиокниги", author: "Артур Шопенгауэр", lengthSeconds: 14689),
+    AudioFile(title: "Good song", album: "Some Album", author: "Some author", lengthSeconds: 1633)
+])
+
+// Проиграть 1 песню (и вспомнить, что выходное устройство не подключено :-))
+
+myAudioPlayer.playCurrentComposition() // можно также playNext()
+
+// Подключить к проигрывателю колонки Loudspeakers.
+
+let loudspeakers = Loudspeakers()
+myAudioPlayer.setOutputDevice(loudspeakers)
+
+// Перемешать песни (метод shuffleCompositions())
+
+myAudioPlayer.shuffleCompositions()
+
+// Проиграть 5 песен через метод playNext()
+
+myAudioPlayer.playNext()
+myAudioPlayer.playNext()
+myAudioPlayer.playNext()
+myAudioPlayer.playNext()
+myAudioPlayer.playNext()
+
+// Вместо колонок подключить наушники Headphones
+
+let headphones = Headphones()
+myAudioPlayer.setOutputDevice(headphones)
+
+// Проиграть 2 песни.
+
+myAudioPlayer.playPrevious()
+myAudioPlayer.playPrevious()
+
+// Опционально: можно отключить выходное устройство
+
+myAudioPlayer.setOutputDevice(nil)
